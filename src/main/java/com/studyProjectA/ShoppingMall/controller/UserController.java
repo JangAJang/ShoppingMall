2acba8c7b1941122b4cdc8fe44199115113ca537
sosow0212 <sosow0212@naver.com>
2022-08-07 19:36:03 +0900

    refactor: change Response type & fixing repository jpa error

--- src/main/java/com/studyProjectA/ShoppingMall/controller/UserController.java
+++ src/main/java/com/studyProjectA/ShoppingMall/controller/UserController.java
@@ -1,8 +1,8 @@
 package com.studyProjectA.ShoppingMall.controller;
 
 import com.studyProjectA.ShoppingMall.dto.RegisterDto;
-import com.studyProjectA.ShoppingMall.response.Response;
 import com.studyProjectA.ShoppingMall.service.UserService;
+import com.studyProjectA.ShoppingMall.response.Response;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -10,28 +10,29 @@ import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api")
 public class UserController {
 
     private final UserService userService;
 
-    @ApiOperation(value="전체 회원 보기 ", notes = "전체 회원을 조회한다. ")
+    @ApiOperation(value = "전체 회원 보기 ", notes = "전체 회원을 조회한다. ")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/users")
-    public Response<?> findAll(){
-        return new Response<>("true", "조회성공", userService.findAll());
+    public Response findAll() {
+        return Response.success(userService.findAll());
     }
 
     @ApiOperation(value = "유저 찾기", notes = "개별 유저 조회")
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/users/{id}")
-    public Response<?> findUser(@PathVariable("id") Integer id){
-        return new Response<>("true", "조회성공", userService.findUser(id));
+    public Response findUser(@PathVariable("id") Integer id) {
+        return Response.success(userService.findUser(id));
     }
 
     @ApiOperation(value = "회원가입", notes = "회원가입 진행")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/auth")
-    public Response<?> register(@RequestBody RegisterDto registerDto){
-        return new Response<>("true", "가입성공", userService.register(registerDto));
+    public Response register(@RequestBody RegisterDto registerDto) {
+        return Response.success(userService.register(registerDto));
     }
 }
