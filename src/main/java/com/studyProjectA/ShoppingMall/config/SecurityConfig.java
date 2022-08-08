6e23d819db4142cb726bc0a8f0c018cff7e9ac40
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-08-08 14:04:40 +0900

    Fix: relocate login Url 로그인 url /login로 재설정, oauth2도 동일 url로 설정.

--- src/main/java/com/studyProjectA/ShoppingMall/config/SecurityConfig.java
+++ src/main/java/com/studyProjectA/ShoppingMall/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.studyProjectA.ShoppingMall.config;
 
+import com.studyProjectA.ShoppingMall.oauth.PrincipalOAuth2UserService;
 import com.studyProjectA.ShoppingMall.repository.UserRepository;
 import com.studyProjectA.ShoppingMall.jwt.JwtAuthenticationFilter;
 import com.studyProjectA.ShoppingMall.jwt.JwtAuthorizationFilter;
@@ -21,6 +22,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     CorsConfig corsConfig;
+
+    @Autowired
+    PrincipalOAuth2UserService principalOAuth2UserService;
 
     @Bean
     public BCryptPasswordEncoder encoder(){
@@ -46,6 +50,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
                 .antMatchers("/admin")
                 .access("hasRole('ROLE_ADMIN')")
-                .anyRequest().permitAll();
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/loginProc")    //로그인 주소 호출되면 security가 낚아챔.
+                .defaultSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .loginPage("/login") // 여기까지만 하면 후처리가 안돼서 로그인 후 창 이동이 안됨.
+                .userInfoEndpoint()
+                .userService(principalOAuth2UserService);
     }
 }
