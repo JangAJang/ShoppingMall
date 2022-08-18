9706123c22e8af0afccecea2ae048c065d679059
username459712 <poll0001@naver.com>
2022-08-18 16:00:43 +0900

    feat:productresponsedto

---
+++ src/main/java/com/studyProjectA/ShoppingMall/dto/ProductResponseDto.java
@@ -0,0 +1,48 @@
+package com.studyProjectA.ShoppingMall.dto;
+
+import com.studyProjectA.ShoppingMall.entity.Product;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductResponseDto {
+    @NotNull
+    private int id;
+
+    @NotNull(message = "제품명을 입력해주세요")
+    private String productName;
+
+    @NotNull
+    private String userName;
+
+    @NotNull(message = "가격을 입력해주세요")
+    private int price;
+
+    @NotNull(message = "수량을 입력해주세요")
+    private int quantity;
+
+    @NotNull(message = "카테고리를 선택해주세요")
+    private String category;
+
+    private long deliveryDate;
+
+    public static ProductResponseDto toDto(Product product){
+        return new ProductResponseDto(
+                product.getId(),
+                product.getProductName(),
+                product.getUser().getUsername(),
+                product.getPrice(),
+                product.getQuantity(),
+                product.getCategory(),
+                product.getDeliveryDate()
+        );
+
+    }
+
+
+}
