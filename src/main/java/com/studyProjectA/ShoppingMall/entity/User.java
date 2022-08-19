b8fd3953c3bc78079cd295ceddac3ba920d0c751
username459712 <poll0001@naver.com>
2022-08-19 19:39:40 +0900

    refactor:int->Long

--- src/main/java/com/studyProjectA/ShoppingMall/entity/User.java
+++ src/main/java/com/studyProjectA/ShoppingMall/entity/User.java
@@ -18,7 +18,7 @@ import java.util.List;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @Column(nullable = false, unique = true)
     private String username;
