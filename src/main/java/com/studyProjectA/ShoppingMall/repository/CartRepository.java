c3c57c8490e99efcff5b96dd860a0d4871265760
username459712 <poll0001@naver.com>
2022-08-19 19:26:36 +0900

    refactor:Integer->Long

--- src/main/java/com/studyProjectA/ShoppingMall/repository/CartRepository.java
+++ src/main/java/com/studyProjectA/ShoppingMall/repository/CartRepository.java
@@ -3,6 +3,6 @@ package com.studyProjectA.ShoppingMall.repository;
 import com.studyProjectA.ShoppingMall.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CartRepository extends JpaRepository<Cart, Integer> {
+public interface CartRepository extends JpaRepository<Cart, Long> {
     Cart findByBuyer(String username);
 }
