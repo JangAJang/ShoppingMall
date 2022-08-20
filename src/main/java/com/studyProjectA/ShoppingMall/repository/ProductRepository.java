6d674d57971a3581335f60e91fc4dffcab7aca33
username459712 <poll0001@naver.com>
2022-08-20 21:24:08 +0900

    feat:productRepository findAllByseller_Username, findAllByproductNameContaining

--- src/main/java/com/studyProjectA/ShoppingMall/repository/ProductRepository.java
+++ src/main/java/com/studyProjectA/ShoppingMall/repository/ProductRepository.java
@@ -2,7 +2,11 @@ package com.studyProjectA.ShoppingMall.repository;
 
 import com.studyProjectA.ShoppingMall.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
     Product findByProductName(String productName);
+    List<Product> findAllBySeller_Username(String userName);
+    List<Product> findAllByProductNameContaining(String searchProductName);
 }
