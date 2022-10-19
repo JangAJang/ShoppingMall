e07743c774a986d128afa73d968144d0417d8d4f
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-10-19 15:11:07 +0900

    refactor: 서비스 기능 세분화

--- src/main/java/com/studyProjectA/ShoppingMall/service/ProductService.java
+++ src/main/java/com/studyProjectA/ShoppingMall/service/ProductService.java
@@ -1,11 +1,13 @@
 package com.studyProjectA.ShoppingMall.service;
 
+import com.studyProjectA.ShoppingMall.dto.ProductDto;
 import com.studyProjectA.ShoppingMall.dto.ProductResponseDto;
 import com.studyProjectA.ShoppingMall.entity.Product;
 import com.studyProjectA.ShoppingMall.entity.User;
+import com.studyProjectA.ShoppingMall.excpetion.ProductNameAlreadyExistsException;
 import com.studyProjectA.ShoppingMall.excpetion.ProductNotFoundException;
-import com.studyProjectA.ShoppingMall.excpetion.UserNotEqualsException;
 import com.studyProjectA.ShoppingMall.excpetion.UserNotFoundException;
+import com.studyProjectA.ShoppingMall.excpetion.UserSellerNotEqualException;
 import com.studyProjectA.ShoppingMall.repository.ProductRepository;
 import com.studyProjectA.ShoppingMall.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -24,97 +26,117 @@ public class ProductService {
     // 전체 물품 조회
     @Transactional(readOnly = true)
     public List<ProductResponseDto> getProducts(){
-        List<Product> products = productRepository.findAll();
-        List<ProductResponseDto> productResponseDto = new ArrayList<>();
-        products.forEach(s-> productResponseDto.add(ProductResponseDto.toDto(s)));
-        return productResponseDto;
+        return changeEntityToDto(getAllProducts());
     }
 
     // 단건 물품 조회
     @Transactional(readOnly = true)
-    public ProductResponseDto getProduct(long id){
+    public ProductResponseDto searchProduct(long id){
         Product product = productRepository.findById(id).orElseThrow(ProductNotFoundException::new);
-
         return ProductResponseDto.toDto(product);
     }
 
     //품목 검색
     @Transactional(readOnly = true)
-    public List<ProductResponseDto> getSearchProducts(String searchProductName){
-
-        List<Product> products = productRepository.findAllByProductNameContaining(searchProductName);
-        if (products.isEmpty())throw new ProductNotFoundException();
-        List<ProductResponseDto> productResponseDtos = new ArrayList<>();
-        products.forEach(s->productResponseDtos.add(ProductResponseDto.toDto(s)));
-        return productResponseDtos;
+    public List<ProductResponseDto> searchProductsByName(String searchProductName){
+        return changeEntityToDto(getProductsByName(searchProductName));
     }
 
     //검색한 사용자 품목 조회
     @Transactional(readOnly = true)
-    public List<ProductResponseDto> getUserProducts(String userName){
-        User user = userRepository.findByUsername(userName).orElseThrow(UserNotFoundException::new);
-        List<Product> products = productRepository.findAllByUser(user);
-        if (products.isEmpty())throw new ProductNotFoundException();
-        List<ProductResponseDto> productResponseDtos = new ArrayList<>();
-        products.forEach(s->productResponseDtos.add(ProductResponseDto.toDto(s)));
-        if(productResponseDtos.isEmpty()) throw new ProductNotFoundException();
-        return productResponseDtos;
+    public List<ProductResponseDto> searchProductsByUsername(String username){
+        User user = getUser(username);
+        List<Product> products = getProductByUser(user);
+        return changeEntityToDto(products);
     }
 
     // 아이템 등록
     @Transactional
-    public ProductResponseDto addProduct(User loginUser, ProductResponseDto productResponseDto){
-        Product product = Product.builder()
-                .productName(productResponseDto.getProductName())
-                .category(productResponseDto.getCategory())
-                .deliveryDate(productResponseDto.getDeliveryDate())
-                .price(productResponseDto.getPrice())
-                .quantity(productResponseDto.getQuantity())
-                .user(loginUser).build();
-        System.out.println(product);
-        productRepository.save(product);
+    public ProductResponseDto addProduct(User loginUser, ProductDto productDto){
+        Product product = setProductByDto(loginUser, new Product(), productDto);
         return ProductResponseDto.toDto(product);
     }
 
     // 아이템 수정
     @Transactional
-    public ProductResponseDto updateProduct(User loginUser, Long itemId, ProductResponseDto productResponseDto) {
-        Product product = productRepository.findById(itemId).orElseThrow(ProductNotFoundException::new);
-        if(loginUser.equals(product.getUser())) {
-            // 토큰 보낸 사람과, 게시글 작성자가 같다면 성공!
-            product.setProductName(productResponseDto.getProductName());
-            product.setPrice(productResponseDto.getPrice());
-            product.setQuantity(productResponseDto.getQuantity());
-            product.setCategory(productResponseDto.getCategory());
-            product.setDeliveryDate(productResponseDto.getDeliveryDate());
-            productRepository.save(product);
-            return ProductResponseDto.toDto(product);
-        } else {
-            // 토큰 보낸 사람과 게시글 작성자가 다르다면 실패!
-            throw new UserNotEqualsException();
-
-        }
+    public ProductResponseDto updateProduct(User loginUser, Long itemId, ProductDto productDto) {
+        Product product = getProduct(itemId);
+        validateUserAuthority(loginUser, product);
+        setProductByDto(loginUser, product, productDto);
+        return ProductResponseDto.toDto(product);
     }
+
 
     // 아이템 삭제
     @Transactional
     public String deleteProduct(User loginUser, Long itemId) {
-        Product product = productRepository.findById(itemId).orElseThrow(ProductNotFoundException::new);
-        if(loginUser.equals(product.getUser())) {
-            productRepository.deleteById(itemId);
-            return "삭제 완료";
-        } else {
-            throw new UserNotEqualsException();
-        }
+        Product product = getProduct(itemId);
+        validateUserAuthority(loginUser, product);
+        productRepository.delete(product);
+        return "삭제 완료";
     }
 
     @Transactional(readOnly = true)
     public List<ProductResponseDto> getMyProducts(User loginUser){
-        List<Product> products = productRepository.findAllByUser(loginUser);
-        if (products.isEmpty())throw new ProductNotFoundException();
+        List<Product> products = getProductByUser(loginUser);
+        return changeEntityToDto(products);
+    }
+
+    public List<Product> getAllProducts(){
+        List<Product> products = productRepository.findAll();
+        validateProductExistence(products);
+        return products;
+    }
+
+    public List<ProductResponseDto> changeEntityToDto(List<Product> products){
         List<ProductResponseDto> productResponseDtos = new ArrayList<>();
-        products.forEach(s->productResponseDtos.add(ProductResponseDto.toDto(s)));
-        if(productResponseDtos.isEmpty()) throw new ProductNotFoundException();
+        products.forEach(s-> productResponseDtos.add(ProductResponseDto.toDto(s)));
         return productResponseDtos;
+    }
+
+    public void validateProductExistence(List<Product> products){
+        if(products.isEmpty()) throw new ProductNotFoundException();
+    }
+
+    public void validateUserAuthority(User loginUser, Product product){
+        if(!loginUser.equals(product.getUser())) throw new UserSellerNotEqualException();
+    }
+
+    public void validateProductNameExistence(String productName){
+        for(Product product : getAllProducts()){
+            if(product.getProductName().equals(productName)) throw new ProductNameAlreadyExistsException();
+        }
+    }
+
+    public List<Product> getProductsByName(String name){
+        List<Product> products = productRepository.findAllByProductNameContaining(name);
+        validateProductExistence(products);
+        return products;
+    }
+
+    public List<Product> getProductByUser(User user){
+        List<Product> products = productRepository.findAllByUser(user);
+        validateProductExistence(products);
+        return products;
+    }
+
+    public User getUser(String username){
+        return userRepository.findByUsername(username).orElseThrow(UserNotFoundException::new);
+    }
+
+    public Product getProduct(Long id){
+        return productRepository.findById(id).orElseThrow(ProductNotFoundException::new);
+    }
+
+    public Product setProductByDto(User user, Product product, ProductDto productDto){
+        validateProductNameExistence(productDto.getProductName());
+        product.setProductName(productDto.getProductName());
+        product.setQuantity(productDto.getQuantity());
+        product.setCategory(productDto.getCategory());
+        product.setDeliveryDate(productDto.getDeliveryDate());
+        product.setPrice(productDto.getPrice());
+        product.setUser(user);
+        productRepository.save(product);
+        return product;
     }
 }
