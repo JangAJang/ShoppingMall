7b0e67a4f1246f6ad59aeeefaaef7cd838332dac
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-24 10:28:52 +0900

    refactor: JwtAuthorization jwt헤더가 인증되지 않았을 때 처리하는 기능을 메서드 분리

--- src/main/java/com/studyProjectA/ShoppingMall/jwt/JwtAuthorizationFilter.java
+++ src/main/java/com/studyProjectA/ShoppingMall/jwt/JwtAuthorizationFilter.java
@@ -20,7 +20,7 @@ import java.io.IOException;
 
 public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     public JwtAuthorizationFilter(AuthenticationManager authenticationManager, UserRepository userRepository) {
         super(authenticationManager);
@@ -30,14 +30,14 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException{
         String jwtHeader = request.getHeader(JwtProperties.HEADER_STRING);
-        System.out.println(jwtHeader); //header 확인 로그
-        if(jwtHeader == null || !jwtHeader.startsWith(JwtProperties.TOKEN_PREFIX)){
-            System.out.println("Jwt Filter did not work with JWT token");
+        printJWTHeader(jwtHeader);
+        if(isJwtHeaderNotValidated(jwtHeader)){
+            System.out.println("[ERROR] Jwt Filter did not work with JWT token");
             chain.doFilter(request, response);
             return;
         }
-        String jwtToken = request.getHeader(JwtProperties.HEADER_STRING).replace(JwtProperties.TOKEN_PREFIX, "");
-        String username = JWT.require(Algorithm.HMAC512(JwtProperties.SECRET)).build().verify(jwtToken).getClaim("username").asString();
+        String username = JWT.require(Algorithm.HMAC512(JwtProperties.SECRET))
+                .build().verify(replaceWordOnToken(request)).getClaim("username").asString();
         if(username!=null){
             User tmpEntity = userRepository.findByUsername(username).orElseThrow(UserNotFoundException::new);
             PrincipalDetails principalDetails = new PrincipalDetails(tmpEntity);
@@ -45,5 +45,17 @@ public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
             SecurityContextHolder.getContext().setAuthentication(authentication);
             chain.doFilter(request, response);
         }
+    }
+
+    private void printJWTHeader(String jwtHeader){
+        System.out.println(jwtHeader);
+    }
+
+    private String replaceWordOnToken(HttpServletRequest request){
+        return request.getHeader(JwtProperties.HEADER_STRING).replace(JwtProperties.TOKEN_PREFIX, "");
+    }
+
+    private boolean isJwtHeaderNotValidated(String jwtHeader){
+        return jwtHeader == null || !jwtHeader.startsWith(JwtProperties.TOKEN_PREFIX);
     }
 }
